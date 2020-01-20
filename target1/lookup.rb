0f2eb7e59df3059440a6b01cafcc4cd85fbc21c4
praveen-hb <59311248+praveen-hb@users.noreply.github.com>
2020-01-20 22:23:59 +0530

    revising milestone 1

--- target1/lookup.rb
+++ target1/lookup.rb
@@ -12,35 +12,32 @@ def get_command_line_argument
   dns_raw = File.readlines("zone")
  
 def parse_dns(dns_raw)
-    #Comment lines and empty lines are rejected
-    records=dns_raw.filter {|record| record[0]!="#" and !record.strip.empty?}
-    #Each record divided into 3 parts
-    records=records.map {|record| record.split(",")}
-    #extra white spaces are removed
-    records.each do |record|
+    dns_records=dns_raw.filter {|record| record[0]!="#" and !record.strip.empty?}
+    dns_records=dns_records.map {|record| record.split(",")}
+    dns_records.each do |record|
         record=record.map {|element| element.strip}
     end
-       # In each record of records
+       # In each record of dns_records
           #   record[0]=type of record
           #   record[1]=old domain name
           #   record[2]=new domain name or IP address
 
-    records
+    dns_records
 end
 
 def resolve(dns_records,lookup_chain,domain)
     #checking domain is present in A records or not
-    answer=dns_records.find {|record| record[1]==domain && record[0]=="A"}
+    lookup_result=dns_records.find {|record| record[1]==domain && record[0]=="A"}
     #if domain not present in A records
-    if answer==nil
+    if lookup_result==nil
         #checking domain is present in CNAME records
-        answer=dns_records.find {|record| record[1]==domain && record[0]=="CNAME"}
+        lookup_result=dns_records.find {|record| record[1]==domain && record[0]=="CNAME"}
         #if domain not present in CNAME records
-        if answer==nil
+        if lookup_result==nil
             puts "Error: record not found for #{domain}"
             exit
         else
-            domain=answer[2]
+            domain=lookup_result[2]
             #cycle checking
             cycle_domain=lookup_chain.find {|domain_name| domain_name==domain}
             #if current domain already present in lookup chain
@@ -49,12 +46,12 @@ def resolve(dns_records,lookup_chain,domain)
                 exit
             end
             #push the new NAME of domain and search
-            lookup_chain.push(answer[2])
+            lookup_chain.push(lookup_result[2])
             lookup_chain=resolve(dns_records,lookup_chain,domain)
         end
     else
         #push IP address 
-        lookup_chain.push(answer[2])
+        lookup_chain.push(lookup_result[2])
     end
     lookup_chain
 end
