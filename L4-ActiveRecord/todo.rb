a7e525ee2ff6ef249c5181e6ffe56c1bf294e135
praveen-hb <praveenudu9@gmail.com>
2020-02-20 23:11:43 +0530

    As required

---
+++ L4-ActiveRecord/todo.rb
@@ -0,0 +1,39 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def self.add_task(todo)
+    create(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+  end
+
+  def due_today?
+    Date.today == due_date
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts where("due_date < ?", Date.today).map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts where(due_date: Date.today).map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts where("due_date > ?", Date.today).map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = find(todo_id)
+    todo.update(completed: true)
+    todo
+  end
+end
